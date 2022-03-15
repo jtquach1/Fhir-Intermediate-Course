cbda00c658b851dc53b58abd26fa2c21cfc1c256
jtquach1 <jtquach1@gmail.com>
2022-03-15 12:30:32 -0400

    Finish L01-1

--- Week 2/fhirclient_js/client/L01_1_fetch_demographics.js
+++ Week 2/fhirclient_js/client/L01_1_fetch_demographics.js
@@ -1,11 +1,64 @@
 const Client = require("fhir-kit-client");
 module.exports = { GetPatientPhoneAndEmail };
 
+// Copied from L00_0_demo.js
+async function GetPatient(
+  server,
+  patientidentifiersystem,
+  patientidentifiervalue
+) {
+  const fhirClient = new Client({
+    baseUrl: server,
+  });
+
+  var PatientInfo = null;
+  let searchResponse = await fhirClient.search({
+    resourceType: "Patient",
+    searchParams: {
+      identifier: patientidentifiersystem + "|" + patientidentifiervalue,
+    },
+  });
+  entries = searchResponse.entry;
+  if (entries) {
+    PatientInfo = entries[0].resource;
+  }
+  return PatientInfo;
+}
+
+const capitalize = (word) =>
+  word.charAt(0).toLocaleUpperCase() + word.substr(1);
+
+const format = (array) => (array.length === 0 ? "-" : array.join(","));
+
 async function GetPatientPhoneAndEmail(
   server,
   patientidentifiersystem,
   patientidentifiervalue
 ) {
-  var aux = "";
-  return aux;
+  const patient = await GetPatient(
+    server,
+    patientidentifiersystem,
+    patientidentifiervalue
+  );
+
+  if (patient) {
+    const phones = [];
+    const emails = [];
+
+    if (patient.telecom) {
+      for (const contactPoint of patient.telecom) {
+        const { system, value, use } = { ...contactPoint };
+        if (system && value && use) {
+          const item = `${value}(${capitalize(use)})`;
+          if (system === "phone") phones.push(item);
+          if (system === "email") emails.push(item);
+        }
+      }
+    }
+
+    const output = `Emails:${format(emails)}\nPhones:${format(phones)}\n`;
+    return output;
+  }
+
+  return "Error:Patient_Not_Found";
 }
--- Week 2/fhirclient_js/client/config.js
+++ Week 2/fhirclient_js/client/config.js
@@ -2,12 +2,14 @@ const ServerEndpoint = () => {
   return "http://wildfhir4.aegis.net/fhir4-0-1";
 };
 const TerminologyServerEndpoint = () => {
-  // Doesn't work because it doesn't seem like results.data.expansion exists
+  // Doesn't work? 1st one doesn't because results.data.expansion doesn't exist
+  return "https://snowstorm.ihtsdotools.org/fhir";
   // return "https://snowstorm-alpha.ihtsdotools.org/fhir";
+  // return "https://dev-snowstorm.ihtsdotools.org/fhir";
   // AU-based
   // return "https://r4.ontoserver.csiro.au/fhir";
   // US-based
-  return "http://tx.fhir.org/r4";
+  // return "http://tx.fhir.org/r4";
 };
 const AssignmentSubmissionFHIRServer = () => {
   return "http://fhirserver.hl7fundamentals.org/fhir/";
