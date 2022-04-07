f647477d1f6d6dc48bfe6420d9b8a5887698b9f2
jtquach1 <jtquach1@gmail.com>
2022-04-07 14:19:36 -0400

    Run project through Prettier

--- Week 3/Material tools-20220324/fhir-server/dbconfig.js
+++ Week 3/Material tools-20220324/fhir-server/dbconfig.js
@@ -1,10 +1,10 @@
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('database', 'username', 'password', {
-    dialect: 'sqlite',
-    storage: './persondb.db',
-    dialectOptions: {
-        // Your sqlite3 options here
-    }
+var Sequelize = require("sequelize");
+var sequelize = new Sequelize("database", "username", "password", {
+  dialect: "sqlite",
+  storage: "./persondb.db",
+  dialectOptions: {
+    // Your sqlite3 options here
+  },
 });
 
-module.exports.db = sequelize
+module.exports.db = sequelize;
--- Week 3/Material tools-20220324/fhir-server/legacy_document_type.js
+++ Week 3/Material tools-20220324/fhir-server/legacy_document_type.js
@@ -3,43 +3,41 @@
 //at the mapper level
 //based on a configuration file
 //We read this mapping from a json file
-const options = require('./fhir_options.json');
-//Two functions 
+const options = require("./fhir_options.json");
+//Two functions
 //One of them returns the legacy type given the FHIR system for the identifier
 function GetDocumentType(system) {
-    var type = "";
-    const DocTypeToSystemMap = options.SystemMap;
-    var MyIndex = -1;
-    for (let i = 0; i < DocTypeToSystemMap.length; i++) {
-        if (DocTypeToSystemMap[i].system == system) {
-            MyIndex = i;
-            break;
-        }
-    };
-    if (MyIndex > -1) {
-        type = DocTypeToSystemMap[MyIndex].type;
+  var type = "";
+  const DocTypeToSystemMap = options.SystemMap;
+  var MyIndex = -1;
+  for (let i = 0; i < DocTypeToSystemMap.length; i++) {
+    if (DocTypeToSystemMap[i].system == system) {
+      MyIndex = i;
+      break;
     }
-    return type;
+  }
+  if (MyIndex > -1) {
+    type = DocTypeToSystemMap[MyIndex].type;
+  }
+  return type;
 }
 //The other returns use and system for each legacy document type
 function GetDocumentSystemUse(abbrev) {
-
-    var system = "";
-    var use = "";
-    const DocTypeToSystemMap = options.SystemMap;
-    var MyIndex = -1;
-    for (let i = 0; i < DocTypeToSystemMap.length; i++) {
-        if (DocTypeToSystemMap[i].type == abbrev) {
-            MyIndex = i;
-            break;
-        }
-    };
-    if (MyIndex > -1) {
-        system = DocTypeToSystemMap[MyIndex].system;
-        use = DocTypeToSystemMap[MyIndex].use;
+  var system = "";
+  var use = "";
+  const DocTypeToSystemMap = options.SystemMap;
+  var MyIndex = -1;
+  for (let i = 0; i < DocTypeToSystemMap.length; i++) {
+    if (DocTypeToSystemMap[i].type == abbrev) {
+      MyIndex = i;
+      break;
     }
-    return { system, use };
-
+  }
+  if (MyIndex > -1) {
+    system = DocTypeToSystemMap[MyIndex].system;
+    use = DocTypeToSystemMap[MyIndex].use;
+  }
+  return { system, use };
 }
 
-module.exports = { GetDocumentSystemUse, GetDocumentType }
+module.exports = { GetDocumentSystemUse, GetDocumentType };
