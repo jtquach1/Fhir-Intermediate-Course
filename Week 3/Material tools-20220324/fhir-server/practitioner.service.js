c56e75e7ae90e681cecca80a8e76aa2a53364114
jtquach1 <jtquach1@gmail.com>
2022-04-09 00:06:19 -0400

    Begin refactoring Patient and Practitioner services and modify Postman collection

--- Week 3/Material tools-20220324/fhir-server/practitioner.service.js
+++ Week 3/Material tools-20220324/fhir-server/practitioner.service.js
@@ -1,5 +1,5 @@
 /*eslint no-unused-vars: "warn"*/
-// We only support one resource type: patient. And only search by id, by specific criteria and create.
+// We only support one resource type: practitioner. And only search by id, by specific criteria and create.
 
 // Housekeeping for sequelize
 const { DataTypes } = require("sequelize");
@@ -13,38 +13,21 @@ const DocType = require("./models/DOC_TYPE");
 // Mapping between FHIR system and legacy document type
 const LegacyDocumentType = require("./legacy_document_type");
 
-// UID generator for bundles
-const uuidv4 = require("uuid").v4;
-
-// FHIR specific stuff: Server, resources: Patient, Bundle, OperationOutcome and Entry
-const { RESOURCES } = require("@asymmetrik/node-fhir-server-core").constants;
-const FHIRServer = require("@asymmetrik/node-fhir-server-core");
-const getPatient = require("@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/patient");
-const getBundle = require("@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/bundle");
+// FHIR specific stuff: Server, resources: Practitioner, Bundle, OperationOutcome and Entry
 const getOperationOutcome = require("@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/operationoutcome");
-const getBundleEntry = require("@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/bundleentry");
-const getPractitioner = require("@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/practitioner");
 
-// Meta data for FHIR R4
-let getMeta = (base_version) => {
-  return require(FHIRServer.resolveFromVersion(base_version, RESOURCES.META));
-};
+const {
+  getBaseUrl,
+  getPersonsByIdentifier,
+  convertLegacyToFhirIdentifiers,
+  personToPatientOrPractitionerMapper,
+  getPatientsOrPractitioners,
+} = require("./serviceUtils");
 
-// How to search the address of our server, so we can return it in the fullURL for each Patient entry
-function getBaseUrl(context) {
-  const FHIRVersion = "/4_0_0/";
-  var protocol = "http://";
-  if (context.req.secure) {
-    protocol = "https://";
-  }
-  const baseUrl = `${protocol}${context.req.headers.host}${FHIRVersion}`;
-  return baseUrl;
-}
-
-// This is for patient searches (direct read is special, below)
+// This is for practitioner searches (direct read is special, below)
 module.exports.search = (args, context, logger) =>
   new Promise((resolve, reject) => {
-    //	logger.info('Patient >>> search');
+    //	logger.info('Practitioner >>> search');
     let baseUrl = getBaseUrl(context);
 
     const {
@@ -195,338 +178,39 @@ module.exports.search = (args, context, logger) =>
           criteria.push(item);
         });
 
-        // Now with the complete criteria, search all the patients and assemble the bundle
-        getPatients(person, include, criteria, context, _count, _page).then(
-          (result) => {
-            resolve(result);
-          }
-        );
+        // Now with the complete criteria, search all the patients/practitioners and assemble the bundle
+        getPatientsOrPractitioners(
+          person,
+          include,
+          criteria,
+          context,
+          _count,
+          _page,
+          "Practitioner"
+        ).then((result) => {
+          resolve(result);
+        });
       });
     } else {
       // Normal search using all the criteria but 'identifier'
-      getPatients(person, include, criteria, context, _count, _page).then(
-        (result) => {
-          resolve(result);
-        }
-      );
-    }
-  });
-
-// This function/promise returns an array of sequelize criteria with the legacy person id with a specific document type/number
-function getPersonsByIdentifier(personDoc, docType, searchType, searchValue) {
-  return new Promise(function (resolve, reject) {
-    // Empty array of person id's
-    const persons = [];
-
-    // Special type of identifier: "ID" because it's not really an identifier
-    // It's the server assigned ID
-    if (searchType == "ID") {
-      persons.push({ PRSN_ID: searchValue });
-      resolve(persons);
-    } else {
-      // Association between DOC_TYPE and PERSON_DOC to search by the abbreviated type and not by ID
-      let include = [
-        {
-          model: docType,
-          as: "DOC_TYPE",
-        },
-      ];
-
-      if (searchType != "") {
-        include.where = [{ DCTP_ABREV: searchType }];
-      }
-
-      // Criteria involves the document number
-      const criteria = [];
-      criteria.push({ PRDT_DOC_VALUE: searchValue });
-
-      // Here we ask for all the persons matching the criteria
-      personDoc
-        .findAll({
-          where: criteria,
-          include: include,
-        })
-        .then((personDocs) => {
-          personDocs.forEach((personDoc) => {
-            // And add them to the criteria array
-            persons.push({ PRSN_ID: personDoc.PRDT_PRSN_ID });
-          });
-
-          if (persons.length == 0) {
-            // tricky: there was no person we add something that will always fail
-            // in a autonumeric INT, to ensure that we will return no
-            // patient at all
-            persons.push({ PRSN_ID: -1 });
-          }
-
-          // And that's our completed job
-          resolve(persons);
-        });
-    }
-  });
-}
-
-// This is the specific search for all patients matching the query
-function getPatients(person, include, criteria, context, count = 5, page = 1) {
-  return new Promise(function (resolve, reject) {
-    // Here we solve paginations issues: how many records per page, which page
-    const offset = (page - 1) * count;
-    const limit = count;
-
-    // Bundle and Entry definitions
-    let BundleEntry = getBundleEntry;
-    let Bundle = getBundle;
-
-    // Our Base address
-    let baseUrl = getBaseUrl(context);
-
-    result = [];
-    entries = [];
-
-    // Get total number of rows because we want to know how many records in total we have to report that in our searchset bundle
-    person
-      .findAndCountAll({
-        where: criteria,
-        include: include,
-        distinct: true,
-      })
-      .then((TotalCount) => {
-        // Adjust page offset and limit to the total count
-        if (offset + limit > TotalCount) {
-          limit = count;
-          offset = 0;
-        }
-
-        // Now we actually do the search combining the criteria, inclusions, limit and offset
-        person
-          .findAll({
-            where: criteria,
-            include: include,
-            limit: limit,
-            offset: offset,
-          })
-          .then((persons) => {
-            persons.forEach((person) => {
-              // We map from legacy person to patient
-              const initialPatient = personToPatientMapper(person);
-
-              // Add the identifiers
-              const patient = personIdentifierToPatientIdentifierMapper(
-                initialPatient,
-                person
-              );
-
-              // And save the result in an array
-              result.push(patient);
-            });
-
-            // With all the patients we have in the result.array we assemble the entries
-            const entries = result.map(
-              (patient) =>
-                new BundleEntry({
-                  fullUrl: `${baseUrl}/Patient/${patient.id}`,
-                  resource: patient,
-                })
-            );
-
-            // We assemble the bundle with the type, total, entries, id, and meta
-            const bundle = new Bundle({
-              id: uuidv4(),
-              meta: {
-                lastUpdated: new Date(),
-              },
-              type: "searchset",
-              total: TotalCount.count,
-              entry: entries,
-            });
-
-            // And finally, we generate the link element
-            // self (always), prev (if there is a previous page available)
-            // next (if there is a next page available)
-            var OriginalQuery = `${baseUrl}Patient`;
-            var LinkQuery = `${baseUrl}Patient`;
-            var parNum = 0;
-            var linkParNum = 0;
-
-            // This is to reassemble the query
-            for (var param in context.req.query) {
-              console.log(param);
-              console.log(context.req.query[param]);
-              if (param != "base_version") {
-                var sep = "&";
-                parNum = parNum + 1;
-
-                if (parNum == 1) {
-                  sep = "?";
-                }
-
-                OriginalQuery = `${OriginalQuery}${sep}${param}=${context.req.query[param]}`;
-
-                if (param != "_page" && param != "_count") {
-                  var LinkSep = "&";
-                  linkParNum = linkParNum + 1;
-
-                  if (linkParNum == 1) {
-                    LinkSep = "?";
-                  }
-
-                  LinkQuery = `${LinkQuery}${LinkSep}${param}=${context.req.query[param]}`;
-                }
-              }
-            }
-
-            // self is always there
-            MyLinks = [
-              {
-                relation: "self",
-                url: OriginalQuery,
-              },
-            ];
-
-            // prev and next may or not exist
-            if (page > 1) {
-              const prevPage = page - 1;
-              MyLinks.push({
-                relation: "prev",
-                url: `${LinkQuery}&_count=${count}&_page=${prevPage.toString()}`,
-              });
-            }
-
-            MaxPages = TotalCount.count / count + 1;
-            MaxPages = parseInt(MaxPages);
-
-            if (page < MaxPages) {
-              const nextPage = page + 1;
-              MyLinks.push({
-                relation: "next",
-                url: `${LinkQuery}&_count=${count}&_page=${nextPage.toString()}`,
-              });
-            }
-
-            bundle.link = MyLinks;
-
-            // Now we have all the required elements, so we can return the complete bundle
-            resolve(bundle);
-          });
+      getPatientsOrPractitioners(
+        person,
+        include,
+        criteria,
+        context,
+        _count,
+        _page,
+        "Practitioner"
+      ).then((result) => {
+        resolve(result);
       });
+    }
   });
-}
 
-// Person to Patient mapper. This function receives a legacy person and returns a FHIR Patient
-function personToPatientMapper(person) {
-  let fhirPatient = new getPatient();
-
-  if (person) {
-    // Logical server id
-    fhirPatient.id = person.PRSN_ID.toString();
-
-    // We only have family, given and text. If we have more than one given, we will adjust later.
-    fhirPatient.name = [
-      {
-        use: "official",
-        family: person.PRSN_LAST_NAME,
-        given: [person.PRSN_FIRST_NAME],
-
-        text: `${person.PRSN_FIRST_NAME} ${person.PRSN_LAST_NAME}`,
-      },
-    ];
-
-    // Mapping of gender is not needed because it's the same codes
-    fhirPatient.gender = person.PRSN_GENDER;
-
-    // BirthDate no conversion needed
-    fhirPatient.birthDate = person.PRSN_BIRTH_DATE;
-
-    // If there is second name then we add the given and adjust the text element
-    if (person.PRSN_SECOND_NAME != "") {
-      fhirPatient.name[0].given.push(person.PRSN_SECOND_NAME);
-      fhirPatient.name[0].text =
-        fhirPatient.name.text = `${person.PRSN_FIRST_NAME} ${person.PRSN_SECOND_NAME} ${person.PRSN_LAST_NAME}`;
-    }
-
-    // We map our legacy identifier type to FHIR system
-    let legacyMapper = LegacyDocumentType;
-    mapper = legacyMapper.GetDocumentSystemUse("ID");
-
-    // We have the identifier (use, system, value)
-    fhirPatient.identifier = [
-      {
-        use: mapper.use,
-        system: mapper.system,
-        value: person.PRSN_ID.toString(),
-        period: { start: person.createdAt },
-      },
-    ];
-
-    // We assemble the email address
-    fhirPatient.telecom = [
-      {
-        system: "email",
-        value: person.PRSN_EMAIL,
-      },
-    ];
-
-    // If there is a nick name, we add it
-    if (person.PRSN_NICK_NAME != "") {
-      legal_name = fhirPatient.name[0];
-      fhirPatient.name = [
-        legal_name,
-        {
-          use: "nickname",
-          given: [person.PRSN_NICK_NAME],
-        },
-      ];
-    }
-
-    // Full text for the resource. NO automatic narrative.
-    fhirPatient.text = {
-      status: "generated",
-      div:
-        '<div xmlns="http:// www.w3.org/1999/xhtml">' +
-        fhirPatient.name[0].text +
-        "</div>",
-    };
-  }
-
-  // And that's our resource
-  return fhirPatient;
-}
-
-// Providing special support for the person's identifiers
-function personIdentifierToPatientIdentifierMapper(fhirPatient, person) {
-  // Our helper for transforming the legacy to system/value
-  let legacyMapper = LegacyDocumentType;
-  MyDocs = person.PERSON_DOC;
-
-  if (MyDocs) {
-    // For each legacy identifier
-    MyDocs.forEach((doc) => {
-      var docTypeCode = doc.DOC_TYPE.DCTP_ABREV;
-      var docNumber = doc.PRDT_DOC_VALUE;
-      var startDate = doc.createdAt;
-      var mapped = legacyMapper.GetDocumentSystemUse(docTypeCode);
-
-      if (mapped.system != "") {
-        // Assemble each identifier
-        // use-system-value-period
-        var oldCol = fhirPatient.identifier;
-        oldCol.push({
-          use: mapped.use,
-          system: mapped.system,
-          value: docNumber,
-          period: { start: startDate },
-        });
-        fhirPatient.identifier = oldCol;
-      }
-    });
-    return fhirPatient;
-  }
-}
-
-// POST of a new Patient Instance
+// POST of a new Practitioner Instance
 module.exports.create = (args, context, logger) =>
   new Promise((resolve, reject) => {
-    // 	logger.info('Patient >>> searchById');
+    // 	logger.info('Practitioner >>> searchById');
     let { base_version } = args;
 
     // Our legacy model
@@ -593,14 +277,14 @@ module.exports.create = (args, context, logger) =>
           }
         });
 
-        // This is all the information that the response will have about the patient, the newId in Location
+        // This is all the information that the response will have about the practitioner, the newId in Location
         resolve({ id: newId });
       });
   });
 
 module.exports.searchById = (args, context, logger) =>
   new Promise((resolve, reject) => {
-    // 	logger.info('Patient >>> searchById');
+    // 	logger.info('Practitioner >>> searchById');
     let { base_version, id } = args;
     let person = new Person(sequelize, DataTypes);
     let personDoc = new PersonDoc(sequelize, DataTypes);
@@ -631,17 +315,20 @@ module.exports.searchById = (args, context, logger) =>
       })
       .then((person) => {
         if (person) {
-          const initialPatient = personToPatientMapper(person);
-          const patient = personIdentifierToPatientIdentifierMapper(
-            initialPatient,
+          const initialPractitioner = personToPatientOrPractitionerMapper(
+            person,
+            "Practitioner"
+          );
+          const practitioner = convertLegacyToFhirIdentifiers(
+            initialPractitioner,
             person
           );
-          resolve(patient);
+          resolve(practitioner);
         } else {
           let operationOutcome = new getOperationOutcome();
           let legacyMapper = LegacyDocumentType;
           var mapped = legacyMapper.GetDocumentSystemUse("ID");
-          var message = `Patient with identifier ${mapped.system} ${id} not found `;
+          var message = `Practitioner with identifier ${mapped.system} ${id} not found `;
 
           operationOutcome.issue = [
             {
---
+++ Week 3/Material tools-20220324/fhir-server/serviceUtils.js
@@ -0,0 +1,377 @@
+/*eslint no-unused-vars: "warn"*/
+// Mapping between FHIR system and legacy document type
+const LegacyDocumentType = require("./legacy_document_type");
+
+// UID generator for bundles
+const uuidv4 = require("uuid").v4;
+
+// FHIR specific stuff: Server, resources: Patient, Bundle, OperationOutcome and Entry
+const { RESOURCES } = require("@asymmetrik/node-fhir-server-core").constants;
+const FHIRServer = require("@asymmetrik/node-fhir-server-core");
+const getBundle = require("@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/bundle");
+const getBundleEntry = require("@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/bundleentry");
+const getPatient = require("@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/patient");
+const getPractitioner = require("@asymmetrik/node-fhir-server-core/src/server/resources/4_0_0/schemas/practitioner");
+
+// Meta data for FHIR R4
+let getMeta = (base_version) => {
+  return require(FHIRServer.resolveFromVersion(base_version, RESOURCES.META));
+};
+
+// How to search the address of our server, so we can return it in the fullURL for each Patient entry
+function getBaseUrl(context) {
+  const FHIRVersion = "/4_0_0/";
+  var protocol = "http://";
+  if (context.req.secure) {
+    protocol = "https://";
+  }
+  const baseUrl = `${protocol}${context.req.headers.host}${FHIRVersion}`;
+  return baseUrl;
+}
+
+// This function/promise returns an array of sequelize criteria with the legacy person id with a specific document type/number
+function getPersonsByIdentifier(personDoc, docType, searchType, searchValue) {
+  return new Promise(function (resolve, reject) {
+    // Empty array of person id's
+    const persons = [];
+
+    // Special type of identifier: "ID" because it's not really an identifier
+    // It's the server assigned ID
+    if (searchType == "ID") {
+      persons.push({ PRSN_ID: searchValue });
+      resolve(persons);
+    } else {
+      // Association between DOC_TYPE and PERSON_DOC to search by the abbreviated type and not by ID
+      let include = [
+        {
+          model: docType,
+          as: "DOC_TYPE",
+        },
+      ];
+
+      if (searchType != "") {
+        include.where = [{ DCTP_ABREV: searchType }];
+      }
+
+      // Criteria involves the document number
+      const criteria = [];
+      criteria.push({ PRDT_DOC_VALUE: searchValue });
+
+      // Here we ask for all the persons matching the criteria
+      personDoc
+        .findAll({
+          where: criteria,
+          include: include,
+        })
+        .then((personDocs) => {
+          personDocs.forEach((personDoc) => {
+            // And add them to the criteria array
+            persons.push({ PRSN_ID: personDoc.PRDT_PRSN_ID });
+          });
+
+          if (persons.length == 0) {
+            // tricky: there was no person we add something that will always fail
+            // in a autonumeric INT, to ensure that we will return no
+            // patient/practitioner at all
+            persons.push({ PRSN_ID: -1 });
+          }
+
+          // And that's our completed job
+          resolve(persons);
+        });
+    }
+  });
+}
+
+// Providing special support for the person's identifiers
+function convertLegacyToFhirIdentifiers(resource, person) {
+  // Our helper for transforming the legacy to system/value
+  let legacyMapper = LegacyDocumentType;
+  MyDocs = person.PERSON_DOC;
+
+  if (MyDocs) {
+    // For each legacy identifier
+    MyDocs.forEach((doc) => {
+      var docTypeCode = doc.DOC_TYPE.DCTP_ABREV;
+      var docNumber = doc.PRDT_DOC_VALUE;
+      var startDate = doc.createdAt;
+      var mapped = legacyMapper.GetDocumentSystemUse(docTypeCode);
+
+      if (mapped.system != "") {
+        // Assemble each identifier
+        // use-system-value-period
+        var oldCol = resource.identifier;
+        oldCol.push({
+          use: mapped.use,
+          system: mapped.system,
+          value: docNumber,
+          period: { start: startDate },
+        });
+        resource.identifier = oldCol;
+      }
+    });
+    return resource;
+  }
+}
+
+// Person to Patient mapper. This function receives a legacy person and returns a FHIR Patient
+function personToPatientOrPractitionerMapper(person, resourceType) {
+  let resource;
+  if (resourceType === "Patient") {
+    resource = new getPatient();
+  } else {
+    resource = new getPractitioner();
+  }
+
+  if (person) {
+    if (resourceType === "Practitioner" && !person.NPI) {
+      return;
+    }
+
+    // Logical server id
+    resource.id = person.PRSN_ID.toString();
+
+    // We only have family, given and text. If we have more than one given, we will adjust later.
+    resource.name = [
+      {
+        use: "official",
+        family: person.PRSN_LAST_NAME,
+        given: [person.PRSN_FIRST_NAME],
+
+        text: `${person.PRSN_FIRST_NAME} ${person.PRSN_LAST_NAME}`,
+      },
+    ];
+
+    // Mapping of gender is not needed because it's the same codes
+    resource.gender = person.PRSN_GENDER;
+
+    // BirthDate no conversion needed
+    resource.birthDate = person.PRSN_BIRTH_DATE;
+
+    // If there is second name then we add the given and adjust the text element
+    if (person.PRSN_SECOND_NAME != "") {
+      resource.name[0].given.push(person.PRSN_SECOND_NAME);
+      resource.name[0].text =
+        resource.name.text = `${person.PRSN_FIRST_NAME} ${person.PRSN_SECOND_NAME} ${person.PRSN_LAST_NAME}`;
+    }
+
+    // We map our legacy identifier type to FHIR system
+    let legacyMapper = LegacyDocumentType;
+    const idMapper = legacyMapper.GetDocumentSystemUse("ID");
+    const npiMapper = legacyMapper.GetDocumentSystemUse("NPI");
+    const npiIdentifier = person.NPI && {
+      use: npiMapper.use,
+      system: npiMapper.system,
+      value: person.NPI?.toString(),
+      period: { start: person.createdAt },
+    };
+
+    // We have the identifier (use, system, value)
+    resource.identifier = [
+      {
+        use: idMapper.use,
+        system: idMapper.system,
+        value: person.PRSN_ID.toString(),
+        period: { start: person.createdAt },
+      },
+      npiIdentifier,
+    ].filter((value) => !!value);
+
+    // We assemble the email address
+    resource.telecom = [
+      {
+        system: "email",
+        value: person.PRSN_EMAIL,
+      },
+    ];
+
+    // If there is a nick name, we add it
+    if (person.PRSN_NICK_NAME != "") {
+      legal_name = resource.name[0];
+      resource.name = [
+        legal_name,
+        {
+          use: "nickname",
+          given: [person.PRSN_NICK_NAME],
+        },
+      ];
+    }
+
+    // Full text for the resource. NO automatic narrative.
+    resource.text = {
+      status: "generated",
+      div:
+        '<div xmlns="http:// www.w3.org/1999/xhtml">' +
+        resource.name[0].text +
+        "</div>",
+    };
+  }
+
+  // And that's our resource
+  return resource;
+}
+
+// This is the specific search for all patients matching the query
+function getPatientsOrPractitioners(
+  person,
+  include,
+  criteria,
+  context,
+  count = 5,
+  page = 1,
+  resourceType
+) {
+  return new Promise(function (resolve, reject) {
+    // Here we solve paginations issues: how many records per page, which page
+    const offset = (page - 1) * count;
+    const limit = count;
+
+    // Bundle and Entry definitions
+    let BundleEntry = getBundleEntry;
+    let Bundle = getBundle;
+
+    // Our Base address
+    let baseUrl = getBaseUrl(context);
+
+    result = [];
+    entries = [];
+
+    // Get total number of rows because we want to know how many records in total we have to report that in our searchset bundle
+    person
+      .findAndCountAll({
+        where: criteria,
+        include: include,
+        distinct: true,
+      })
+      .then((TotalCount) => {
+        // Adjust page offset and limit to the total count
+        if (offset + limit > TotalCount) {
+          limit = count;
+          offset = 0;
+        }
+
+        // Now we actually do the search combining the criteria, inclusions, limit and offset
+        person
+          .findAll({
+            where: criteria,
+            include: include,
+            limit: limit,
+            offset: offset,
+          })
+          .then((persons) => {
+            persons.forEach((person) => {
+              // We map from legacy person to person/practitioner
+              const initialResource = personToPatientOrPractitionerMapper(
+                person,
+                resourceType
+              );
+
+              // Add the identifiers
+              const resource = convertLegacyToFhirIdentifiers(
+                initialResource,
+                person
+              );
+
+              // And save the result in an array
+              result.push(resource);
+            });
+
+            // With all the patients we have in the result.array we assemble the entries
+            const entries = result.map(
+              (resource) =>
+                new BundleEntry({
+                  fullUrl: `${baseUrl}/${resourceType}/${resource.id}`,
+                  resource: resource,
+                })
+            );
+
+            // We assemble the bundle with the type, total, entries, id, and meta
+            const bundle = new Bundle({
+              id: uuidv4(),
+              meta: {
+                lastUpdated: new Date(),
+              },
+              type: "searchset",
+              total: TotalCount.count,
+              entry: entries,
+            });
+
+            // And finally, we generate the link element
+            // self (always), prev (if there is a previous page available)
+            // next (if there is a next page available)
+            var OriginalQuery = `${baseUrl}${resourceType}`;
+            var LinkQuery = `${baseUrl}${resourceType}`;
+            var parNum = 0;
+            var linkParNum = 0;
+
+            // This is to reassemble the query
+            for (var param in context.req.query) {
+              console.log(param);
+              console.log(context.req.query[param]);
+              if (param != "base_version") {
+                var sep = "&";
+                parNum = parNum + 1;
+
+                if (parNum == 1) {
+                  sep = "?";
+                }
+
+                OriginalQuery = `${OriginalQuery}${sep}${param}=${context.req.query[param]}`;
+
+                if (param != "_page" && param != "_count") {
+                  var LinkSep = "&";
+                  linkParNum = linkParNum + 1;
+
+                  if (linkParNum == 1) {
+                    LinkSep = "?";
+                  }
+
+                  LinkQuery = `${LinkQuery}${LinkSep}${param}=${context.req.query[param]}`;
+                }
+              }
+            }
+
+            // self is always there
+            MyLinks = [
+              {
+                relation: "self",
+                url: OriginalQuery,
+              },
+            ];
+
+            // prev and next may or not exist
+            if (page > 1) {
+              const prevPage = page - 1;
+              MyLinks.push({
+                relation: "prev",
+                url: `${LinkQuery}&_count=${count}&_page=${prevPage.toString()}`,
+              });
+            }
+
+            MaxPages = TotalCount.count / count + 1;
+            MaxPages = parseInt(MaxPages);
+
+            if (page < MaxPages) {
+              const nextPage = page + 1;
+              MyLinks.push({
+                relation: "next",
+                url: `${LinkQuery}&_count=${count}&_page=${nextPage.toString()}`,
+              });
+            }
+
+            bundle.link = MyLinks;
+
+            // Now we have all the required elements, so we can return the complete bundle
+            resolve(bundle);
+          });
+      });
+  });
+}
+
+module.exports = {
+  getBaseUrl,
+  getPersonsByIdentifier,
+  convertLegacyToFhirIdentifiers,
+  personToPatientOrPractitionerMapper,
+  getPatientsOrPractitioners,
+};
